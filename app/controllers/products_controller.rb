0545db305b318e5a575ca07e347d1525b918d1b7
Civil21 <diusolexander@gmail.com>
2019-11-17 16:16:13 +0200

    add images to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,7 @@
 class ProductsController < InheritedResources::Base
-
   private
 
-    def product_params
-      params.require(:product).permit(:name, :desc, :price)
-    end
-
+  def product_params
+    params.require(:product).permit(:name, :desc, :price, images: [])
+  end
 end
