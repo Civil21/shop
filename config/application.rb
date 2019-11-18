db732806ddc2d89618ce44decd882d55d09fb2c8
Civil21 <diusolexander@gmail.com>
2019-11-18 14:40:54 +0200

    refix product

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,7 +12,7 @@ module Shop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.app_generators.scaffold_controller = :scaffold_controller
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :products
   root 'products#index'
 
-  resources :products
 
   resources :comments, only: %i[create update destroy]
 
