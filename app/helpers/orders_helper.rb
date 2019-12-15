70049da97b2efc3b3f51ca29e50c704de53ed94a
Civil21 <diusolexander@gmail.com>
2019-12-15 15:07:10 +0200

    fix order

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -5,9 +5,9 @@ module OrdersHelper
     case order.state
     when 'active'
       [
-        link_to( 'Підтвердити замовлення', state_order_path(order.id, 'order'), method: :post),
-        link_to( '| Видалити', order_path(order.id), method: :delete)
-      ].join(" ").html_safe
+        link_to('Підтвердити замовлення', state_order_path(order.id, 'order'), method: :post),
+        link_to('| Видалити', order_path(order.id), method: :delete)
+      ].join(' ').html_safe
     when 'order'
       if user_signed_in? && current_user.id == 1
         link_to 'Підтвердити відпралвення', state_order_path(order.id, 'sent'), method: :post
@@ -18,12 +18,12 @@ module OrdersHelper
       if user_signed_in? && current_user.id == 1
         link_to 'Відмінити', state_order_path(order.id, 'canceled'), method: :post
       else
-        link_to 'Підтвердити доставку', order_path(order.id), method: :delete
+        link_to 'Підтвердити доставку', state_order_path(order.id, 'delivered'), method: :post
       end
     when 'canceled'
-      "Відмінено #{order.updated_at.strftime("%d %b, %Y o %H:%M")}"
+      "Відмінено #{order.updated_at.strftime('%d %b, %Y o %H:%M')}"
     when 'delivered'
-      "Доставленно #{order.updated_at.strftime("%d %b, %Y o %H:%M")}"
+      "Доставленно #{order.updated_at.strftime('%d %b, %Y o %H:%M')}"
     end
   end
 end
